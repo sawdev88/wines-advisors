bd0effee8335e43af1abe1c2a41564f4f77d555d
Sean Wines <seanwinesdev@gmail.com>
2020-04-22 10:48:59 -0700

    resp design

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -7,12 +7,12 @@ function About() {
       <h2 className="title">About</h2>
 
       <div className="d-flex mt-4">
-        <div className="w-50 mt-4 content">
+        <div className="flex-1 mt-4 content">
           <p>Wines Advisors was formed in 2020 to bring best in class Business Brokerage and Mergers and Acquisitions services to the Sacramento Regional Area.  Our principals have completed over 100 successful business transactions, within the business sales and commercial real estate fields, with total consideration of several hundred million dollars.  Our clients have ranged from local privately held businesses to globally recognized private equity firms.  We are solely focused on business sales and our aim is to sell your businesses quickly, confidentially, and for top dollar.</p>
 
           <p>Many of our opportunities are not openly marketed due to confidentiality concerns.  If you would like to preview our deal flow, please fill out the contact form below.</p>
         </div>
-        <div className="w-50 position-relative">
+        <div className="flex-1 position-relative">
           <div className="img-border"></div>
           <img className="about-img" src={placeholder} alt="placeholder" />
         </div>
